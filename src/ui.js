9f66b154458acc44391130b04db8106302fc7a13
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-03-10 15:42:12 -0500

    add edit button image

--- src/ui.js
+++ src/ui.js
@@ -2,6 +2,7 @@ import { manager } from "./tasks";
 import { saveTasks } from "./storage";
 import { createProjectList } from "./sidebar";
 import deleteButton from "./images/x-square.svg";
+import editButton from "./images/edit.svg";
 
 const deleteSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#89a9a9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>';
 
@@ -109,16 +110,22 @@ function appendToList(task, taskIndex) {
         taskItem.checked = task.completed;
     }
 
-
+    // DELETE BUTTON
     const imgDelete = document.createElement('img');
     imgDelete.src = deleteButton;
-    imgDelete.classList = 'taskDelete';
+    imgDelete.classList = 'taskImg';
     imgDelete.addEventListener('click', () => {
         if (confirm('Delete task? (THIS IS PERMANENT)')){
             manager.deleteTask(taskItem.getAttribute('taskIndex'));
             refreshTaskList();
         }
     });
+
+    // EDIT BUTTON
+    const imgEdit = document.createElement('img');
+    imgEdit.src = editButton;
+    imgEdit.classList = 'taskImg';
+    //edit task code
 
     taskItem.addEventListener('change', () => {
         manager.getTasks()[taskItem.getAttribute('taskIndex')].toggle();
@@ -127,6 +134,7 @@ function appendToList(task, taskIndex) {
 
     taskDiv.appendChild(taskItem);
     taskDiv.appendChild(taskLabel);
+    taskDiv.appendChild(imgEdit);
     taskDiv.appendChild(imgDelete);
     if (task.completed){
         listComplete.appendChild(taskDiv);
