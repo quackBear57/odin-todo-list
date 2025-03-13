85961e3b8f8d5e432b1e9f50712e32537bf47b33
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-03-13 09:24:57 -0500

    don't allow blank edits

--- src/ui.js
+++ src/ui.js
@@ -4,8 +4,6 @@ import { createProjectList } from "./sidebar";
 import deleteButton from "./images/x-square.svg";
 import editButton from "./images/edit.svg";
 
-const deleteSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#89a9a9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-square"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>';
-
 export function createForm() {
     const taskListContainer = document.querySelector("#taskListContainer");
 
@@ -219,16 +217,24 @@ function displayEdit(taskIndex){
         inputEditTitle.classList = 'taskInput';
         inputEditTitle.addEventListener('keydown', (event) => {
             if (event.key === 'Enter'){
-                manager.editTask(index,inputEditTitle.value);
-                refreshTaskList();
+                if (inputEditTitle.value === '') {
+                    inputEditTitle.placeholder = 'Edit task name - cannot be blank';
+                } else {
+                    manager.editTask(index,inputEditTitle.value);
+                    refreshTaskList();
+                }
             }
         });
 
         const buttonEdit = document.createElement('button');
         buttonEdit.classList = 'editSaveButton';
         buttonEdit.addEventListener('click', () => {
-            manager.editTask(index,inputEditTitle.value);
-            refreshTaskList();
+            if (inputEditTitle.value === '') {
+                inputEditTitle.placeholder = 'Edit task name - cannot be blank';
+            } else {
+                manager.editTask(index,inputEditTitle.value);
+                refreshTaskList();
+            }
         });
         buttonEdit.textContent = 'Save';
 
